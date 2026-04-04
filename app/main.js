e0449a0484e65922a6b92e81bc805ca2da1821d6
anamaldo <amaldonadomenendez@gmail.com>
2026-04-04 02:58:32 +0200

    Funciona el sistema base de Phaser y esta todo organizado

--- app/main.js
+++ app/main.js
@@ -1,1 +1,34 @@
 console.log("happy coding")
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container', 
+    backgroundColor: '#000000',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Aquí cargo assets para el juego, como imágenes, sonidos, etc. 
+
+}
+
+function create() {
+    // Aquí creo los objetos del juego, como sprites, texto, etc.
+    this.add.text(100, 100, '¡happy coding!', { font: '48px Arial', fill: '#ffffff' });
+    
+    
+    const rect = this.add.graphics();
+
+    rect.fillStyle(0xffff00, 1)
+
+    rect.fillRect(200, 150, 400, 300);
+    
+    rect.lineStyle(5, 0xffffff, 1);
+    rect.strokeRect(200, 150, 400, 300);
+}
